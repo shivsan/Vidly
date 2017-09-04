de949908947f6c4924b93153d5cebd72b837e03e
shivsan <shivku.goku@gmail.com>
2017-09-04 22:32:18 +0530

    Commit all

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute("Movies", 
+            //    "movies/releases/{year}/{genre}", 
+            //    new { controller="movies", action="byrelease", year="year", genre="genre"},
+            //    constraints: new { year=@"\d{4}", genre=@"[a-z]*"});
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    [RoutePrefix("movies")]
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+
+            return Content(string.Format("{0}{1}", pageIndex, sortBy));
+        }
+
+        [Route("releases/{year:regex(\\d{4})}/{genre:regex(\\w+)}")]
+        public ActionResult ByRelease(int year, string genre)
+        {
+            ViewBag.Title = "";
+            ViewData[""] = "";
+            ViewData["faf"] = "";
+            return Content(string.Format("{0} {1}", year, genre));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+id);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie { Id = 1, Name = "Shrek" };
+            dynamic dyn = new System.Dynamic.ExpandoObject();
+            dyn.f = "";
+            ViewData["title"] = "big";
+            return View(movie);
+            //return new ViewResult();
+        }
+
+        public ActionResult HalfMovie()
+        {
+            var movie = new Movie { Id = 1, Name = "Shrek" };
+            return PartialView(movie);
+            //return new ViewResult();
+        }
+
+        public JsonResult GetData()
+        {
+            return new JsonResult();
+        }
+
+        public HttpNotFoundResult Nothing()
+        {
+            return new HttpNotFoundResult();
+        }
+
+        public ContentResult Cont()
+        {
+            return new ContentResult() { Content = "Shrekking" };
+        }
+
+    }
+}
